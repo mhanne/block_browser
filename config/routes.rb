c9c3aff62521c98fc1658b01fff597237cbe8263
Marius Hanne <marius.hanne@sourceagency.org>
2016-07-17 16:43:16 +0200

    only enable namecoin specific routes when needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 BlockBrowser::Application.routes.draw do
 
-  get 'upload' => 'blocks#upload'
   get 'blocks' => 'blocks#index', as: :blocks
   get 'block/:id' => 'blocks#show', as: :block
   get 'tx/:id' => 'tx#show', as: :tx
@@ -10,14 +9,13 @@ BlockBrowser::Application.routes.draw do
   get 'script' => 'scripts#show'
   get 'address/:id/unspent' => 'address#unspent', as: :address_unspent
   get 'address/:id' => 'address#show', as: :address
+
   get 'search/:search' => 'search#search'
   get 'search' => 'search#search', as: :search
+
   get 'unconfirmed' => 'blocks#unconfirmed', as: :unconfirmed
   get 'scripts/:type' => 'scripts#index', as: :scripts
   get 'p2sh_scripts/:type' => 'scripts#p2sh_index', as: :p2sh_scripts
-  get 'names' => 'names#index', as: :names
-  get 'name/heights/*name' => 'names#heights', as: :name_heights
-  get 'name/*name' => 'names#show', as: :name
 
   get 'relay_tx' => 'relay#relay_tx'
   post 'relay_tx' => 'relay#relay_tx'
@@ -26,7 +24,6 @@ BlockBrowser::Application.routes.draw do
   get 'api/block' => 'api#block', as: :block_api
   get 'api/tx' => 'api#tx', as: :tx_api
   get 'api/address' => 'api#address', as: :address_api
-  get 'api/name' => 'api#name', as: :name_api
   get 'api/relay' => 'api#relay', as: :relay_api
   get 'api/stats' => 'api#stats', as: :stats_api
   get 'api/schema' => 'api#schema', as: :schema_api
@@ -36,6 +33,14 @@ BlockBrowser::Application.routes.draw do
   get 'about' => 'blocks#about', as: :about
   get 'stats' => 'stats#stats', as: :stats
   get 'source' => 'source#source', as: :source
+
+  if Bitcoin.network_project == :namecoin
+    get 'names' => 'names#index', as: :names
+    get 'name/heights/*name' => 'names#heights', as: :name_heights
+    get 'name/*name' => 'names#show', as: :name
+    get 'api/name' => 'api#name', as: :name_api
+  end
+
   root :to => 'blocks#index'
 
 end
